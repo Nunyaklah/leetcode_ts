584208f75b912870533a14e22e5e309689c8c684
Nunyaklah <nunyaklah@gmail.com>
2024-03-05 00:17:04 +0000

    two sum - leetcode 1

--- easy/twoSum.ts
+++ easy/twoSum.ts
@@ -1,1 +1,40 @@
-console.log("hi ts")
+/**
+ *  Leetcode 1 - Two Sum
+ * 
+ *  Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
+
+
+ *  Solution Explanation :
+
+    Map --> {Value : Index}
+
+    We take the: target - num = result , and check if the result is in the map. If its not in the map we put the 
+    number in the map with its index. If its in the map we return is value(index) and the index of the current iteration
+    {
+        
+    }
+
+    [0, 1]
+ */
+
+function twoSum(nums: number[], target: number): number[] {
+  const numMap: Map<number, number> = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const difference = target - nums[i];
+
+    if (numMap.has(difference)) {
+      return [numMap.get(difference)!, i];
+    }
+
+    numMap.set(nums[i], i);
+  }
+
+   return [];
+}
+
+// Time Complexity:  O(n)
+
+console.log(twoSum([2, 7, 11, 15], 9));
